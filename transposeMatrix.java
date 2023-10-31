325d8b623e0613f4493e135818e87976f1c5ee4e
AslamSuneer <103572084+AslamSuneer@users.noreply.github.com>
2023-10-31 11:29:19 +0530

    Update transposeMatrix.java

--- transposeMatrix.java
+++ transposeMatrix.java
@@ -3,9 +3,9 @@ class transpose{
 public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
-System.out.println("Enter the row count: ");
+System.out.println("Enter the row : ");
 int row=sc.nextInt();
-System.out.println("Enter the column count: ");
+System.out.println("Enter the column : ");
 int col=sc.nextInt();
 int a[][]=new int[row][col];
 System.out.println("Enter the elements: ");
